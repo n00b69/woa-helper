119a2301e1eb9629393a8bcf8d6124bde747deab
Rostislav Lastochkin <remtrik@mail.ru>
2025-07-04 19:05:05 +0300

    fyi inverse conditions do exist

--- app/src/main/java/id/kuato/woahelper/widgets/WidgetActivity.java
+++ app/src/main/java/id/kuato/woahelper/widgets/WidgetActivity.java
@@ -68,7 +68,7 @@ public class 	WidgetActivity extends AppCompatActivity {
 		String widget_type = intent.getStringExtra("WIDGET_TYPE");
 
 		MainActivity.context = this;
-		if (Boolean.FALSE.equals(Shell.isAppGrantedRoot())) {
+		if (!Boolean.TRUE.equals(Shell.isAppGrantedRoot())) {
 			Toast.makeText(this, "NO ROOT!", Toast.LENGTH_SHORT).show();
 			finish();
 			return;
