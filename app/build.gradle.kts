be2ea7705ebae9935a425658e534f2de32796928
Victoria Freeman <venddair1@proton.me>
2026-08-18 20:38:29 +0200

    Bumped version to 1.8.7.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 25
         targetSdk = 36
         versionCode = 5
-        versionName = "1.8.7"
+        versionName = "1.8.7.1"
 
         val localesArray = locales.joinToString(", ") { tag ->
             val bcpTag = tag.replace("-r", "-").let { cleanTag ->
